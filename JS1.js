30a1f3118a3dda780261cb9195f487c19befb37e
Tejas <tejasnambiar32@gmail.com>
2021-07-29 17:00:26 +0530

    changed a few lines in js assignments

--- JS1.js
+++ JS1.js
@@ -1,8 +1,8 @@
 //JavaScript program to swap two variables
 
 //take input from the users
-let a = prompt('Enter the first variable: ');
-let b = prompt('Enter the second variable: ');
+let a = prompt('Enter first variable: ');
+let b = prompt('Enter second variable: ');
 
 //create a temporary variable
 let temp;
@@ -12,5 +12,5 @@ temp = a;
 a = b;
 b = temp;
 
-console.log(`The value of a after swapping: ${a}`);
-console.log(`The value of b after swapping: ${b}`);
+console.log(`The value after swapping: ${a}`);
+console.log(`The value after swapping: ${b}`);
